fe02ce9ba0da9d4c7b631eefbe82106d9a3b9efc
misato100 <misato1@my.yorku.ca>
2024-09-24 12:44:52 -0400

    Added a dashboard page (comment unused error)

--- lib/actions.ts
+++ lib/actions.ts
@@ -57,6 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
     } catch (error) {
+      console.log(error);
       return {
         message: 'Database Error: Failed to Create Invoice.',
       };
@@ -90,6 +91,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.log(error);
     return {
       message: 'Database Error: Failed to Update Invoice.',
     };
@@ -108,6 +110,7 @@ export async function deleteInvoice(id: string) {
 
     return { message: 'Deleted Invoice.' };
   } catch (error) {
+    console.log(error);
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     }
